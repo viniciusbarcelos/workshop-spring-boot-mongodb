f2880e9f385f6c77e3ad0e18c45f6a8d123314fe
viniciusbarcelos <vinicius.barcelos@gmail.com>
2020-05-22 20:16:18 -0300

    Obtendo um usuário por id

--- src/main/java/com/vob/workshopmongo/services/UserService.java
+++ src/main/java/com/vob/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.vob.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vob.workshopmongo.domain.User;
 import com.vob.workshopmongo.repository.UserRepository;
+import com.vob.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,12 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id)
+	{
+		Optional<User> user = repo.findById(id); // throws IllegalArgumentException if 'id' is null
+ 
+		return user.orElseThrow(() -> new ObjectNotFoundException(String.format("Requested id=%s does not exist", id)));
+	}
+	
 }
