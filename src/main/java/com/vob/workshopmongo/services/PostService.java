96272b1bb71b830fd2252b43c6c79f59e3b28b31
viniciusbarcelos <vinicius.barcelos@gmail.com>
2020-05-23 02:23:40 -0300

    Obtendo um post por id

---
+++ src/main/java/com/vob/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.vob.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vob.workshopmongo.domain.Post;
+import com.vob.workshopmongo.repository.PostRepository;
+import com.vob.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id); // throws IllegalArgumentException if 'id' is null
+
+		return user.orElseThrow(() -> new ObjectNotFoundException(String.format("Requested id=%s does not exist", id)));
+	}
+
+}
